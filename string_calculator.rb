67ac866965a0d1a33e0fb0b88a63f5d1a1e40d02
Ravi Kumar Bhartiya <rkseven7@hotmail.com>
2025-03-27 16:40:52 +0530

    first commit simple string calc: empty string return 0

---
+++ string_calculator.rb
@@ -0,0 +1,8 @@
+# file string_calculator.rb created
+# Create defination (Green Phase)
+
+class StringCalculator
+  def add(numbers)
+    0
+  end
+end
---
+++ test_string_calculator.rb
@@ -0,0 +1,13 @@
+# file test_string_calculator.rb created
+# NO defination  (Red Phase)
+# Test a simple String calculator with a method signature
+
+require 'test/unit'
+require_relative 'string_calculator'
+
+class TestStringCalculator < Test::Unit::TestCase
+  def test_empty_string_returns_zero
+    calc = StringCalculator.new
+    assert_equal 0, calc.add("")
+  end
+end
