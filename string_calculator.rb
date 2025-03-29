e4e92fc8f4e9a103f1a1a22fcf96cc892078db2e
Ravi Kumar Bhartiya <rkseven7@hotmail.com>
2025-03-29 12:36:38 +0530

    nums to number_array and remove comments

--- string_calculator.rb
+++ string_calculator.rb
@@ -1,24 +1,24 @@
 class StringCalculator
   def add(numbers)
-    0 if numbers.empty?   # check if the string is empty.
-    delimiter, numbers = parse_demiliter(numbers) # given string "//;\n1;2" identify delimiter, numbers
-    nums = numbers.split(delimiter).map(&:to_i)
-    check_negatives(nums)
-    nums.sum
+    0 if numbers.empty?
+    delimiter, numbers = parse_demiliter(numbers)
+    number_array = numbers.split(delimiter).map(&:to_i)
+    check_negatives(number_array)
+    number_array.sum
   end
 
   private
 
   def parse_demiliter(numbers)
-    if numbers.start_with?("//") # enter if custom delimiter "//" start_with
-      delimiter = numbers[2]     # at index 2 identifies delimiter ";"
-      numbers = numbers.split("\n", 2) [1] # separate delimiter newline for [1;2]
+    if numbers.start_with?("//")
+      delimiter = numbers[2]
+      numbers = numbers.split("\n", 2) [1] 
       return [delimiter, numbers]
     end
     [/,|\n/, numbers]
   end
-  def check_negatives(nums)
-    negatives = nums.select { |n| n < 0 } # get all negatives at on place
+  def check_negatives(number_array)
+    negatives = number_array.select { |n| n < 0 } # get all negatives at on place
     raise "Negatives not allowed: #{negatives.join(', ')}" unless negatives.empty?
   end
 end
